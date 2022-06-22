71cd2baede79a89fb41562566a6a9c97f67f6c47
Lauri <lauri.kalliola97@outlook.com>
2022-06-22 21:33:06 +0300

    Added bigMob and Boss prefabs

---
+++ Assets/Code/Health and Damage/EnemyHealth.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace GamecampPeli
+{
+    public class EnemyHealth : MonoBehaviour, IDamageable
+    {
+        [SerializeField] private int health = 1;
+
+        // The DealDamage function of the IDamageable interface with
+        // required components and functions for damaging the enemy
+        // and removing the enemy from existence when it reaches
+        // 0 health
+        public void DealDamage(int damage)
+        {
+            health -= damage;
+
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
