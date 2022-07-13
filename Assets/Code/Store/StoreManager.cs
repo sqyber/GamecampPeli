d7e77db4ed1e4e36a633fafd8f2c96e73668a240
Samu <sqyber@gmail.com>
2022-07-13 20:14:12 +0300

    added useless + and - buttons  to shop script still wip

--- Assets/Code/Store/StoreManager.cs
+++ Assets/Code/Store/StoreManager.cs
@@ -1,21 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace GamecampPeli
 {
     public class StoreManager : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
+
+        [SerializeField] private int MaxHpBonus = 10;
+        [SerializeField] int CurrentHpBonus = 1;
 
         // Update is called once per frame
-        void Update()
+        void Start () {
+     
+        }
+
+        public void Update()
         {
-        
+
+        }
+
+        public void IncreaseValue()
+            {
+                CurrentHpBonus++;
+            }
+
+            public void DecreaseValue()
+            {
+                CurrentHpBonus--;
+            }
+
+
         }
     }
-}
