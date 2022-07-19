0535057e492a5208069fc363811ee65285d6a3e9
Samu <sqyber@gmail.com>
2022-07-19 18:49:51 +0300

    Limiters WIP in healthbonus.cs

--- Assets/Code/Store/HealthBonus.cs
+++ Assets/Code/Store/HealthBonus.cs
@@ -15,12 +15,17 @@ namespace GamecampPeli
 
         private TextMeshProUGUI textMesh;
         private ShopCurrencyManager currencyManager;
+        
+        
 
         // Update is called once per frame
         void Start ()
         {
             textMesh = gameObject.GetComponent<TextMeshProUGUI>();
             currencyManager = GetComponent<ShopCurrencyManager>();
+            
+            //Instantiate(currencyManager.SavedCurrency);
+            
         }
         public void Update()
         {
@@ -29,19 +34,35 @@ namespace GamecampPeli
             Limiter();
             }
         
-
+        //increase the upgrade level
         public void IncreaseValue()
             {
-                currentHpBonus++;
-                PlayerPrefs.SetInt("Coins", currencyManager.SavedCurrency-10 * currentHpBonus);
+                if (currencyManager.SavedCurrency >10* currentHpBonus)
+                {
+                    currentHpBonus++;
+                    currencyManager.SavedCurrency -= 10 * currentHpBonus;
+                }
+                else
+                {
+                    return;
+                }
+                
             }
-
+        //decrease upgrade level
         public void DecreaseValue()
+        {
+            if (currentHpBonus > 1)
             {
                 currentHpBonus--;
-                PlayerPrefs.SetInt("Coins", currencyManager.SavedCurrency+10 * currentHpBonus);
+                currencyManager.SavedCurrency += 10 * currentHpBonus;
             }
-
+            else
+            {
+                return;
+            }
+                
+            }
+        //limit the upgrade levels to given values
         public void Limiter()
             {
                 if (currentHpBonus > maxHpBonus)
